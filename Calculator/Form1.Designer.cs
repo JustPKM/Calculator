53b6c650432daa9c7d8aa4efedeb9bc80470efdc
JustPKM <phankhanhminh1502@gmail.com>
2023-10-18 21:19:02 +0700

    Add operator button

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -41,6 +41,14 @@
             this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -137,7 +145,7 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(93, 65);
             this.button10.TabIndex = 1;
-            this.button10.Text = "button1";
+            this.button10.Text = "-/+";
             this.button10.UseVisualStyleBackColor = true;
             // 
             // button11
@@ -155,14 +163,94 @@
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(93, 65);
             this.button12.TabIndex = 1;
-            this.button12.Text = "button1";
+            this.button12.Text = ".";
             this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(331, 112);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(93, 65);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "C";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(450, 112);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(93, 65);
+            this.button14.TabIndex = 3;
+            this.button14.Text = "AC";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(331, 193);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(93, 65);
+            this.button15.TabIndex = 2;
+            this.button15.Text = "+";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(450, 193);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(93, 65);
+            this.button16.TabIndex = 3;
+            this.button16.Text = "-";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(331, 274);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(93, 65);
+            this.button17.TabIndex = 2;
+            this.button17.Text = "x";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(450, 274);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(93, 65);
+            this.button18.TabIndex = 3;
+            this.button18.Text = "/";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(331, 354);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(93, 65);
+            this.button19.TabIndex = 4;
+            this.button19.Text = "=";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(450, 354);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(93, 65);
+            this.button20.TabIndex = 5;
+            this.button20.Text = "%";
+            this.button20.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(571, 431);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button13);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button6);
@@ -198,6 +286,14 @@
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
     }
 }
 
